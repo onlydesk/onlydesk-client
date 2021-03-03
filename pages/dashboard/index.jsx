397e5e9e5778d7018938018dd604ecc72dca178f
FranciscoMendes10866 <avizinhadochico@gmail.com>
2021-03-03 10:06:38 +0100

    created a navbar for desktop screens, with icons and animations

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from '../../components'
+
+const Dashboard = () => {
+    return (
+        <main className="bg-gray-50 h-screen">
+            <Navbar />
+            <h1>Dashboard</h1>
+        </main>
+    )
+}
+ 
+export default Dashboard
